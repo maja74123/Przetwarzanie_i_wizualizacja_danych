06028cf773f8b164b709775cb16cd5ee682ec3fd
maja74123 <133578971+maja74123@users.noreply.github.com>
2024-01-10 20:33:19 +0100

    Scalono pliki, dodano drobne poprawki (m.in. łamanie linii)

--- Projekt1/Projekt1.R
+++ Projekt1/Projekt1.R
@@ -14,24 +14,29 @@ prepare_dataset_for_project1 <- function(filename, columns_to_remove){
 }
 
 df <- prepare_dataset_for_project1('apartments_pl_2023_08.csv',
-                                   c('floor', 'latitude', 'longitude',
-                                     'ownership','buildingMaterial', 'condition'))
+                                   c('floor', 'latitude',
+                                     'longitude', 'ownership',
+                                     'buildingMaterial', 'condition'))
 View(df)
 
 df$city <- recode(df$city,
-                  "bialystok" = "Białystok", "bydgoszcz" = "Bydgoszcz", "czestochowa" = "Częstochowa",
-                  "gdansk" = "Gdańsk", "gdynia" = "Gdynia", "katowice" = "Katowice",
-                  "krakow" = "Kraków", "lodz" = "Łódź", "lublin" = "Lublin",
-                  "poznan" = "Poznań", "radom" = "Radom", "rzeszow" = "Rzeszów",
-                  "szczecin" = "Szczecin", "warszawa" = "Warszawa", "wroclaw" = "Wrocław")
+                  'bialystok' = 'Białystok', 'bydgoszcz' = 'Bydgoszcz',
+                  'czestochowa' = 'Częstochowa','gdansk' = 'Gdańsk', 
+                  'gdynia' = 'Gdynia', 'katowice' = 'Katowice',
+                  'krakow' = 'Kraków', 'lodz' = 'Łódź',
+                  'lublin' = 'Lublin', 'poznan' = 'Poznań',
+                  'radom' = 'Radom', 'rzeszow' = 'Rzeszów',
+                  'szczecin' = 'Szczecin', 'warszawa' = 'Warszawa',
+                  'wroclaw' = 'Wrocław')
 
 df$type <- recode(df$type,
-                  "apartmentBuilding" = "Apartamentowiec",
-                  "blockOfFlats" = "Blok mieszkalny",
-                  "tenement" = "Kamienica")
+                  'apartmentBuilding' = 'Apartamentowiec',
+                  'blockOfFlats' = 'Blok mieszkalny',
+                  'tenement' = 'Kamienica')
 
-# Dane dotyczące liczby ludności pochodzą z https://stat.gov.pl/download/gfx/portalinformacyjny/pl/defaultaktualnosci/5468/7/20/1/powierzchnia_i_ludnosc_w_przekroju_terytorialnym_w_2023_roku_tablice.xlsx
-data_population <- "city population
+# Dane dotyczące liczby ludności pochodzą z
+# https://stat.gov.pl/download/gfx/portalinformacyjny/pl/defaultaktualnosci/5468/7/20/1/powierzchnia_i_ludnosc_w_przekroju_terytorialnym_w_2023_roku_tablice.xlsx
+data_population <- 'city population
 Białystok 292600
 Bydgoszcz 330038
 Częstochowa 208282
@@ -46,20 +51,26 @@ Radom 197848
 Rzeszów 197181
 Szczecin 391566
 Warszawa 1861975
-Wrocław 674079"
+Wrocław 674079'
 
-df_city_and_population <- as.data.frame(read_delim(data_population, delim = " ", col_names = TRUE, trim_ws = TRUE))
+df_city_and_population <- as.data.frame(read_delim(data_population,
+                                                   delim = ' ',
+                                                   col_names = TRUE,
+                                                   trim_ws = TRUE))
 
-df_with_population <- merge(x = df, y = df_city_and_population, by = 'city', all = TRUE)
+df_with_population <- merge(x = df, y = df_city_and_population,
+                            by = 'city', all = TRUE)
 
 # Tworzenie pliku zawierającego df z populacją
-# write.csv(df_with_population, "apartments_pl_2023_08_with_population.csv", row.names = TRUE)
+# write.csv(df_with_population,
+# 'apartments_pl_2023_08_with_population.csv', row.names = TRUE)
 
-# Wykresy punktowe pokazujące zależność między powierzchnią mieszkań a rokiem budowy i typem budynku (z podziałem na miasta)
+# Wykresy punktowe pokazujące zależność między powierzchnią mieszkań
+# a rokiem budowy i typem budynku (z podziałem na miasta)
 ggplot(df, aes(x = buildYear, y = squareMeters, color = type)) +
   geom_point(alpha = 0.5) +
   facet_wrap(~city) +
-  ggtitle("Powierzchnia mieszkań w zależności od roku budowy i typu budynku")+
+  ggtitle('Powierzchnia mieszkań w zależności od roku budowy i typu budynku') +
   labs(x = 'Rok budowy',
        y = 'Powierzchnia [m\u00B2]',
        color = 'Typ budynku') +
@@ -83,42 +94,61 @@ total_count_without_elevator <- df %>%
   group_by(floorCount) %>%
   summarise('Frequency' = sum(hasElevator == 'no'))
 
-# Wykres kolumnowy pokazujący liczbę mieszkań posiadających windę w zależności od liczby pięter
-buildings_with_elevator <- ggplot(as.data.frame(count_buildings_with_elevator), aes(x = floorCount, y = Frequency, fill = type)) +
+# Wykres kolumnowy pokazujący liczbę mieszkań posiadających windę
+# w zależności od liczby pięter
+buildings_with_elevator <- ggplot(as.data.frame(count_buildings_with_elevator),
+                                  aes(x = floorCount,
+                                      y = Frequency,
+                                      fill = type)) +
   geom_col() +
   ggtitle('Liczba mieszkań posiadających windę w zależności od liczby pięter z podziałem na typ budynku') +
   labs(x = 'Liczba pięter',
        y = 'Liczba mieszkań',
        fill = 'Typ budynku') +
   scale_x_continuous(breaks = 1:20, limits=c(0, 21)) +
-  geom_text(aes(label = ifelse(Frequency >= 58, Frequency, '')), size = 2.6, position = position_stack(vjust = 0.5)) +
+  geom_text(aes(label = ifelse(Frequency >= 35, Frequency, '')),
+            size = 2.6, position = position_stack(vjust = 0.5)) +
   geom_text(inherit.aes = FALSE, data = total_count_with_elevator, 
-            aes(x = floorCount, y = Frequency, label = Frequency), size = 2.9, vjust = -0.5)
+            aes(x = floorCount, y = Frequency, label = Frequency),
+            size = 2.9, vjust = -0.5)
 
-# Wykres kolumnowy pokazujący liczbę mieszkań nieposiadających windy w zależności od liczby pięter
-buildings_without_elevator <- ggplot(as.data.frame(count_buildings_without_elevator), aes(x = floorCount, y = Frequency, fill = type)) +
+# Wykres kolumnowy pokazujący liczbę mieszkań nieposiadających windy
+# w zależności od liczby pięter
+buildings_without_elevator <- ggplot(as.data.frame(count_buildings_without_elevator),
+                                     aes(x = floorCount,
+                                         y = Frequency,
+                                         fill = type)) +
   geom_col() +
   ggtitle('Liczba mieszkań nieposiadających windy w zależności od liczby pięter z podziałem na typ budynku') +
   labs(x = 'Liczba pięter',
        y = 'Liczba mieszkań',
        fill = 'Typ budynku') +
   scale_x_continuous(breaks = 1:20, limits=c(0, 21)) +
-  geom_text(aes(label = ifelse(Frequency >= 114, Frequency, '')), size = 2.6, position = position_stack(vjust = 0.5)) +
+  geom_text(aes(label = ifelse(Frequency >= 114, Frequency, '')),
+            size = 2.6, position = position_stack(vjust = 0.5)) +
   geom_text(inherit.aes = FALSE, data = total_count_without_elevator, 
-            aes(x = floorCount, y = Frequency, label = Frequency), size = 2.9, vjust = -0.5)
+            aes(x = floorCount, y = Frequency, label = Frequency),
+            size = 2.9, vjust = -0.5)
 
-# Wyświetlanie dwóch wykresów jeden nad drugim -- wersja z legendą na wykresach (legenda nie zabiera dodatkowego miejsca)
-plot_grid(buildings_with_elevator + theme(legend.background = element_rect(color = 'black', fill = 'grey95'),
+# Wyświetlanie dwóch wykresów jeden nad drugim -- wersja z legendą na wykresach
+# (legenda nie zabiera dodatkowego miejsca)
+plot_grid(buildings_with_elevator +
+            theme(legend.background = element_rect(color = 'black',
+                                                   fill = 'grey95'),
                                           legend.position = c(0.9, 0.8)),
-          buildings_without_elevator + theme(legend.background = element_rect(color = 'black', fill = 'grey95'),
+          buildings_without_elevator +
+            theme(legend.background = element_rect(color = 'black',
+                                                   fill = 'grey95'),
                                              legend.position = c(0.9, 0.8)),
           ncol = 1)
 
 # Przygotowanie danych do wykresów dotyczących ceny za metr kwadratowy
-median_price_per_sqm <- as.data.frame(tapply(df$price / df$squareMeters, df$city, median))
+median_price_per_sqm <- as.data.frame(tapply(df$price / df$squareMeters,
+                                             df$city,
+                                             median))
 
-first_interval <- paste0("(", floor(min(median_price_per_sqm)), ", ", 7000, ")")
-last_interval <- paste0("[", 11500, ", ", ceiling(max(median_price_per_sqm)), ")")
+first_interval <- paste0('(', floor(min(median_price_per_sqm)), ', ', 7000, ')')
+last_interval <- paste0('[', 11500, ', ', ceiling(max(median_price_per_sqm)), ')')
 # Skrajne wartości nie należą do przedziałów, ponieważ obliczono je jako podłogę i sufit.
 
 median_range <- ifelse(median_price_per_sqm < 7000, first_interval,
@@ -126,22 +156,112 @@ median_range <- ifelse(median_price_per_sqm < 7000, first_interval,
                               ifelse(median_price_per_sqm >= 9000 & median_price_per_sqm < 11500, '[9000, 11500)',
                                      last_interval)))
 
-df_city_and_range <- data.frame(city = rownames(median_range), range = unname(median_range))
+df_city_and_range <- data.frame(city = rownames(median_range),
+                                range = unname(median_range))
 
-df_city_and_range$range <- factor(df_city_and_range$range, levels = c(first_interval, '[7000, 9000)', '[9000, 11500)', last_interval))
+df_city_and_range$range <- factor(df_city_and_range$range,
+                                  levels = c(first_interval,
+                                             '[7000, 9000)',
+                                             '[9000, 11500)',
+                                             last_interval))
 
-df_for_boxplots <- merge(x = df[ , c('city', 'price', 'squareMeters')], y = df_city_and_range, by = 'city', all = TRUE)
+df_for_boxplots <- merge(x = df[ , c('city', 'price', 'squareMeters')],
+                         y = df_city_and_range,
+                         by = 'city', all = TRUE)
 
 # Wykresy pudełkowe pokazujące cenę za metr kwadratowy z podziałem na miasta
-ggplot(df_for_boxplots, aes(x = price / squareMeters, y = city, fill = range)) +
+ggplot(df_for_boxplots, aes(x = price / squareMeters,
+                            y = city,
+                            fill = range)) +
   geom_boxplot(alpha = 0.8,
                outlier.size = 2) +
   scale_x_continuous(breaks = 5000 * (1:6)) +
   scale_y_discrete(limits = rev) +
-  scale_fill_manual(values = c( 'orchid', 'goldenrod1' ,'royalblue1', 'olivedrab3')) +
+  scale_fill_manual(values = c( 'orchid',
+                                'goldenrod1',
+                                'royalblue1',
+                                'olivedrab3')) +
   ggtitle('Cena za m\u00B2 z podziałem na miasta') +
   labs(x = 'Cena za m\u00B2 [zł]',
        y = 'Miasto',
        fill = 'Wartość mediany') +
-  theme(legend.background = element_rect(color = 'black', fill = 'grey95'),
+  theme(legend.background = element_rect(color = 'black',
+                                         fill = 'grey95'),
         legend.position = c(0.88,0.89))
+
+# Histogramy pokazujące odległość od mieszkań do najbliższych POI
+# Określenie zakresów osi x i y
+xlim_range <- c(0, 5)
+ylim_range <- c(0, 5500)
+
+# Wektor z nazwami POI
+distance_columns <- c('schoolDistance', 'clinicDistance',
+                      'postOfficeDistance', 'kindergartenDistance',
+                      'restaurantDistance', 'collegeDistance',
+                      'pharmacyDistance')
+
+# Utworzenie nowej tabeli zawierającej wybrane kolumny
+selected_distances <- df[, distance_columns]
+
+# Nazwy dla histogramów
+hist_titles <- c('Szkoła', 'Przychodnia', 'Poczta',
+                 'Przedszkole', 'Restauracja',
+                 'Uniwersytet', 'Apteka')
+
+# Tworzenie wielu histogramów na jednym wykresie
+par(mfrow = c(3, 3), mar = c(5, 4, 2, 0), oma = c(0, 0, 4, 0))
+
+# Tworzenie histogramów
+for (i in 1:length(distance_columns)) {
+  hist_result <- hist(selected_distances[[i]], 
+                      main = paste(hist_titles[i]),
+                      xlab = paste('km'),  
+                      ylab = 'Liczba mieszkań',
+                      col = 'lightblue',
+                      xlim = xlim_range,
+                      ylim = ylim_range)
+  
+  # Dodanie wartości z osi y nad słupkami
+  text(hist_result$mids,
+       hist_result$counts,
+       labels = hist_result$counts,
+       pos = 3,
+       col = '#0000CD',
+       cex = 0.8)
+}
+
+# Tytuł główny
+mtext('Odległość od mieszkań do najbliższych POI (Points Of Interest)',
+      outer = TRUE, line = 1.2, cex = 1.8)
+
+# Wykres typu Lollipop pokazujący liczbę ofert na tysiąc mieszkańców w poszczególnych miastach
+# Wyznaczanie liczby ofert przypadającej na tysiąc mieszkańców
+df_offers_per_1000_people <- df_with_population %>%
+  group_by(city) %>%
+  summarise(total_offers = length(id),
+            total_population = first(population)) %>%
+  mutate(offers_per_1000_people = round((1000 * total_offers / total_population), 2))
+
+# Wykres typu Lollipop
+df_offers_per_1000_people %>%
+  arrange(offers_per_1000_people) %>%
+  mutate(city = factor(city, city)) %>%
+  ggplot(aes(x = offers_per_1000_people, y = city)) +
+  geom_segment(aes(x = 0, xend = offers_per_1000_people,
+                   y = city, yend = city),
+               color = 'grey', linewidth = 1.5) +
+  geom_point(size = 4, color = '#69b3a2') +
+  xlim(0, floor(max(df_offers_per_1000_people$offers_per_1000_people)) + 1) +
+  geom_text(aes(x = offers_per_1000_people,
+                y = city,
+                label = offers_per_1000_people),
+            hjust = -0.4, vjust = 0.3) + # vjust - odległość wartości od lollipopa (góra/dół)
+  theme_minimal() +
+  theme(
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y = element_blank(),
+    plot.title.position = 'plot',
+    plot.title = element_text(hjust = 0.6)) +
+  xlab('Liczba ofert na 1000 mieszkańców') +
+  ylab('Miasto') +
+  ggtitle('Liczba ofert na 1000 mieszkańców w poszczególnych miastach')
